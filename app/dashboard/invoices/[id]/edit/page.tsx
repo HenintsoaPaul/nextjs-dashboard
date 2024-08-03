4eb995dd7d24b22ab3262b9507cb4fdce6e6f5d0
Henintsoa Paul <henintsoapaul@gmail.com>
2024-08-03 12:37:54 +0300

    create-n-update invoice

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/edit-form";
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const id: string = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers()
+    ]);
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    { label: 'Update Invoice', href: `/dashboard/invoices/${id}/edit`, active: true }
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
+}
