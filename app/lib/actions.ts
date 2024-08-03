e7ef335607c00d9b766b56ed2e8d0102d8697100
Henintsoa Paul <henintsoapaul@gmail.com>
2024-08-03 12:39:55 +0300

    delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,7 +18,8 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 const url = '/dashboard/invoices';
 
 export async function deleteInvoice(id: string) {
-    
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath(url);
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
