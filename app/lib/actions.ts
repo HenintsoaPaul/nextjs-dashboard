ff090923d8cc3fae5731754d0e13de9791153551
Henintsoa Paul <henintsoapaul@gmail.com>
2024-08-03 13:34:08 +0300

    try-catch Server Actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,8 +18,13 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 const url = '/dashboard/invoices';
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath(url);
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath(url);
+        return { message: 'Deleted Invoice.' };
+    } catch (err) {
+        return { message: 'Database Error: Failed to delete Invoice.' };
+    }
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
@@ -31,11 +36,14 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}`;
+    } catch (err) {
+        return { message: 'Database Error: Failed to Update Invoice.' }
+    }
 
     revalidatePath(url);
     redirect(url);
@@ -51,10 +59,13 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    } catch (err) {
+        return { message: 'Database Error: Failed to Create Invoice.' }
+    }
 
     revalidatePath(url);
     redirect(url);
